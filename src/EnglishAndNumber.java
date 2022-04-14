1c52ade5e021dccd635a20f633361d53686d993d
HKL <black648@douzone.com>
2022-04-14 17:35:07 +0900

    * 숫자 문자열과 영단어

---
+++ src/EnglishAndNumber.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class EnglishAndNumber {
+    public int solution(String s) {
+
+        Map<String, String> map = new HashMap<>();
+        map.put("zero", "0");
+        map.put("one", "1");
+        map.put("two", "2");
+        map.put("three", "3");
+        map.put("four", "4");
+        map.put("five", "5");
+        map.put("six", "6");
+        map.put("seven", "7");
+        map.put("eight", "8");
+        map.put("nine", "9");
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            if (s.contains(entry.getKey())) {
+                s = s.replaceAll(entry.getKey(), entry.getValue());
+            }
+        }
+        return Integer.parseInt(s);
+    }
+
+
+    public static void main(String[] args) {
+        EnglishAndNumber englishAndNumber = new EnglishAndNumber();
+        System.out.println(englishAndNumber.solution("one4seveneight"));
+    }
+}

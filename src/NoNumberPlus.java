cbcebd1e301b8fff26561688f763a90e935fb081
HKL <black648@douzone.com>
2022-05-04 00:47:34 +0900

    * Lv1. 없는 숫자 더하기

---
+++ src/NoNumberPlus.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class NoNumberPlus {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        for (int i=0; i<10; i++) {
+            int target = i;
+            if (Arrays.stream(numbers).noneMatch(j -> j == target)) {
+                answer += target;
+            }
+        }
+        return answer;
+        //return IntStream.range(0, 10).filter(i -> Arrays.stream(numbers).noneMatch(num -> i == num)).sum();
+
+    }
+
+    public static boolean matched (int i, int[] numbers) {
+        return Arrays.stream(numbers).noneMatch(j -> j == i);
+    }
+
+    public static void main(String[] args) {
+        NoNumberPlus noNumberPlus = new NoNumberPlus();
+        System.out.println(noNumberPlus.solution(new int[] {1,2,3,4,6,7,8,0}));
+    }
+}
